cad93ea57f86acae40009d334c327943637db104
Bai Xueyuan <bai_xueyuan@msn.com>
2017-05-30 21:02:16 +0800

    A small test.
    The goal of using Chinese in R packages has succeeded!

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,11 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+hello <- function(strs) {
+  if (class(strs)=='character' && length(strs)==1) {
+    prt <- paste("你好，", strs, '！', sep='')
+    cat(prt)
+  } else {
+    cat("你好，世界！")
+  }
 }
